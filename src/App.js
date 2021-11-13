bd649727cde2a1019e86c7621d432a2e7f26709d
Mark Otweyo <markotweyo99@gmail.com>
2021-11-13 04:41:13 +0300

    Added Challenge instructions

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Toggle from './components/Toogle'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <Toggle/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Toogle.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+
+
+export const Toogle = () => {
+    handleClick() {
+        // todo
+      }
+    return (
+        <div>
+            <button>ON</button>
+        </div>
+    )
+}
+
+
